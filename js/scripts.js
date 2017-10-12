710dd7a6819f44221753134914d754b150bd01b6
Your Name <you@example.com>
2017-10-12 23:40:13 +0200

    Add files to project

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Enter the length of the base of the triangle');
+var h = prompt('Enter the height of the triangle');
+var triangleArea = a*h/2;
+alert('Triangle field : ' + triangleArea);
+console.log('Hello, ' + name);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
